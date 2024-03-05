fe5e251b943027a820c079f78414d15653e5ebcf
ziyodilloxon <ziyodilloxon8@gmail.com>
2024-03-05 19:03:33 +0500

    i do my h/w

---
+++ js/index.js
@@ -0,0 +1,58 @@
+// 10 misol
+// let a = +prompt("son kiriting");
+// let b = +prompt("Son kiriting");
+// function triangle() {
+//   console.log(a + b - 1);
+// }
+// triangle();
+
+// 11 misol
+
+// let a = +prompt("son kiriting");
+// let b = +prompt("Son kiriting");
+// console.log(a % b);
+
+// 12 misol
+
+// let a = +prompt("to'rtburchakning boyini kiriting");
+// let b = +prompt("to'rtburchakning enini kiriting");
+// function tortburchak() {
+//   console.log((a + b) * 2);
+// }
+// tortburchak();
+
+// 13 misol
+// let a = prompt("So'z kiriting");
+// let b = "Something ";
+// function somthing() {
+//   console.log(b + a);
+// }
+// somthing();
+
+// 14 misol
+// let num = +prompt("sonni kiriting");
+// function kvadrat() {
+//   console.log(num * num);
+// }
+// kvadrat();
+
+// 15 misol
+
+// let num = +prompt("sonni kiriting");
+// let a = 0;
+// function compare() {
+//   if (num <= a) {
+//     console.log("true");
+//   } else {
+//     console.log("false");
+//   }
+// }
+// compare();
+
+// 16 misol
+// let son = +prompt("sonni kiriting");
+
+// function num() {
+//   console.log((son - 2) * 180);
+// }
+// num();
